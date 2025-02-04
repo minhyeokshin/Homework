fd7f079903b23489da7ed30486069b83cfbf3726
minhyeokshin <sinminheyok@gmail.com>
2025-02-04 10:37:25 +0900

    Homework

---
+++ src/HomeWork_250203/Test1_Version2/TestShape.java
@@ -0,0 +1,9 @@
+package HomeWork_250203.Test1_Version2;
+
+
+//public class TestShape {
+//    public static void main(String[] args) {
+//        Shape [] shape = new Shape[2];
+//
+//    }
+//}
---
+++ src/HomeWork_250203/Test3/Prob1.java
@@ -0,0 +1,7 @@
+//package HomeWork_250203.Test3;
+//
+//public class Prob1 {
+//    public static void main(String[] args) {
+//
+//    }
+//}
